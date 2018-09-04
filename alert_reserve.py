86db8d7df37430b7f64c2acc33a574a72869e566
mshrynzw <30368820+mshrynzw@users.noreply.github.com>
2018-09-05 01:53:24 +0900

    Update alert_reserve.py

--- alert_reserve.py
+++ alert_reserve.py
@@ -6,8 +6,11 @@ import bs4
 from time import sleep
 from alert_tweet_reserve import alert_tweet_reserve
 
+list_str = []
+
 while True:
-    sleep(300)
+    sleep(60)
+    print(list_str)
     res = requests.get('https://com.nicovideo.jp/community/co2388471')
     res.raise_for_status()
     soup = bs4.BeautifulSoup(res.text, "html.parser")
@@ -16,4 +19,7 @@ while True:
         str_url = elem.get('href')
         str_url_tmp = r'http://live.nicovideo.jp/gate/lv'
         if str_url.startswith(str_url_tmp):
-            alert_tweet_reserve(str_url.lstrip(str_url_tmp))
+            lv_id = str_url.lstrip(str_url_tmp)
+                if lv_id in list_str
+                    list_str.append(lv_id)
+                    alert_tweet_reserve(lv_id)
