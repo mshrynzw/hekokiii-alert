47f09c26f6a47fbb2d4f721e6773fa889ee59cd2
mshrynzw <30368820+mshrynzw@users.noreply.github.com>
2018-09-19 00:35:58 +0900

    Add files via upload

--- chk_reserve.py
+++ chk_reserve.py
@@ -4,34 +4,39 @@
 import requests
 import bs4
 import os
+import re
 from time import sleep
 from tw_reserve import tweet_reserve_live
 
 def check_reserve_live():
-    list_str = []
+
     community_id = os.environ["NICONICO_COMMUNITY_ID"]
+    listFinishedURL = []
 
     while True:
 
-        #5分毎に予約確認を実施
-        sleep(300)
+        #3分毎に予約確認を実施
+        sleep(180)
         
         #HTMLスクレビング
-        res = requests.get('https://com.nicovideo.jp/community/' + community_id)
+        res = requests.get(r"https://com.nicovideo.jp/community/" + community_id)
         res.raise_for_status()
         soup = bs4.BeautifulSoup(res.text, "html.parser")
-        elems = soup.select('.liveTitle')
-        
-        #<a href>のみ抽出
-        for elem in elems:
-            
-            str_url = elem.get('href')
-            #さらに放送予約URLのみ抽出
-            str_url_tmp = r'http://live.nicovideo.jp/gate/lv'
-            if str_url.startswith(str_url_tmp):
-                
-                lv_id = str_url.lstrip(str_url_tmp)
-                #未ツイートの放送IDのみをツイートする。
-                if not lv_id in list_str:
-                    list_str.append(lv_id)
-                    tweet_reserve_live(lv_id)
+        elemsDate = soup.find_all("span", class_="liveDate", text=re.compile("20.*-"))
+        elemsURL = soup.find_all("a", class_="liveTitle", href=re.compile(
+            r"^http://live.nicovideo.jp/gate/lv"))
+
+        listDate = []
+        listURL = []
+        for elemDate in elemsDate:
+            listDate.append(elemDate.string)
+
+        for elemURL in elemsURL:
+            listURL.append(elemURL.get("href"))
+
+        for i in range(len(listDate)):
+            #未ツイートの放送IDのみをツイートする。
+            if not listURL[i] in listFinishedURL:
+                listFinishedURL.append(listURL)
+                strTweet = "（開始日時：" + listDate[i][:-1].replace(" ","") + "）" + listURL[i]
+                tweet_reserve_live(strTweet)
