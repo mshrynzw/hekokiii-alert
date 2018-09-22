bd53c16e1aed67fb201d3178fc1f0e6af06540db
mshrynzw <30368820+mshrynzw@users.noreply.github.com>
2018-09-22 19:07:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from chk_comment import check_comment
 if __name__ == "__main__":
     thread_1 = threading.Thread(target=check_start_live)
     thread_2 = threading.Thread(target=check_reserve_live)
-    thread_3 = threading.Thread(target=check_comment, args=("315809551",))
+    thread_3 = threading.Thread(target=check_comment, args=("lv315791300",))
     thread_1.start()
     thread_2.start()
     thread_3.start()
