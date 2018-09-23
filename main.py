8d4e2d10a21ba28e3697e60f8cdd668c4f8bfaa9
mshrynzw <30368820+mshrynzw@users.noreply.github.com>
2018-09-23 16:26:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,17 +9,15 @@ q = Queue(connection=conn)
 
 @route('/index')
 def index():
-    result = q.enqueue(background_process, '引数１')
+    result = q.enqueue(background_process)
     return result
 
-def background_process(name):
+def background_process():
     # ここに時間のかかる処理を書く
     if __name__ == "__main__":
         thread_1 = threading.Thread(target=check_start_live)
         thread_2 = threading.Thread(target=check_reserve_live)
         thread_1.start()
         thread_2.start()
-    return name * 10
-
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
