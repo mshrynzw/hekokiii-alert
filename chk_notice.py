dba2de3cd72c131218add06b617cdabbb7baa954
mshrynzw <30368820+mshrynzw@users.noreply.github.com>
2018-10-06 00:40:54 +0900

    Add files via upload

---
+++ chk_notice.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+ 
+import requests
+import bs4
+import os
+import re
+from datetime import datetime, timedelta
+from time import sleep
+from tw_notice import tweet_notice
+
+def check_notice():
+
+    community_id = os.environ["NICONICO_COMMUNITY_ID"]
+    listFinishedNoticeTitle = []
+    listFinishedNoticeText = []
+    yobi = ["月", "火", "水", "木", "金", "土", "日"]
+
+    while True:
+
+        sleep(30)
+        
+        # HTMLスクレビング
+        res = requests.get(r"https://com.nicovideo.jp/community/" + community_id)
+        res.raise_for_status()
+        soup = bs4.BeautifulSoup(res.text, "html.parser")
+        elemsNoticeTitle = soup.find_all("h3", class_="noticeTitle")
+        elemsNoticeDate = soup.find_all("span", class_="date")
+        elemsNoticeText = soup.find_all("p", class_="notice_text")
+
+        listNoticeTitle = []
+        listNoticeDate = []
+        listNoticeText = []
+
+        for elemNoticeTitle in elemsNoticeTitle:
+            listNoticeTitle.append(elemNoticeTitle.string)
+        for elemNoticeDate in elemsNoticeDate:
+            listNoticeDate.append(elemNoticeDate.string)
+        for elemNoticeText in elemsNoticeText:
+            listNoticeText.append(elemNoticeText.string)
+
+        for i in range(len(listNoticeDate)):
+            # 未ツイートのお知らせのみをツイートする。
+            if listNoticeText[i] not in listFinishedNoticeText:
+
+                date1 = listNoticeDate[i][0:10].translate(str.maketrans({"年": "/", "月": "/"}))
+                print(listNoticeDate[i][5:7])
+                print(listNoticeDate[i][8:10])
+                datetime1 = datetime(int(listNoticeDate[i][0:4]), int(listNoticeDate[i][5:7]), int(listNoticeDate[i][8:10]))
+
+                # お知らせ日時が3日以内のみをツイートする。
+                if datetime1 + timedelta(days=3) > datetime.now():
+
+                    listFinishedNoticeTitle.append(listNoticeTitle[i])
+                    listFinishedNoticeText.append(listNoticeText[i])
+
+                    date2 = datetime.strptime(date1, '%Y/%m/%d')
+                    date3 = listNoticeDate[i][5:10].replace(
+                        "月", "/") + "(" + yobi[date2.weekday()] + ")" + listNoticeDate[i][12:17]
+
+                    strTweet =  "【お知らせ】\r " + \
+                                listNoticeTitle[i] + "\r" + \
+                                date3 + "\r" + \
+                                "--------------------------------------------------------------\r" + \
+                                listNoticeText[i]
+                    tweet_notice(strTweet)
+
+        sleep(30)
+
+        for i in range(len(listFinishedNoticeText)):
+            if listFinishedNoticeText[i] not in listNoticeText:
+                del listFinishedNoticeText[i]
---
+++ tw_notice.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+from requests_oauthlib import OAuth1Session
+
+# Twitter用の設定
+CK = os.environ["CONSUMER_KEY"]
+CS = os.environ["CONSUMER_SECRET"]
+AT = os.environ["ACCESS_TOKEN"]
+ATS = os.environ["ACCESS_TOKEN_SECRET"]
+
+# ツイートのテンプレート
+tweet_tpl = os.environ["TWEET_TPL_RESERVE"]
+
+
+def tweet_notice(tweet):
+
+    # 認証処理
+    twitter = OAuth1Session(CK, CS, AT, ATS)
+    # ツイートポストエンドポイント
+    url = "https://api.twitter.com/1.1/statuses/update.json"
+    # post送信
+    params = {"status": tweet}
+    res = twitter.post(url, params=params)
+
+    if res.status_code == 200:  # 正常投稿出来た場合
+        print("【ツイート完了】")
+    else:                       # 正常投稿出来なかった場合
+        print("【ツイート失敗】 : %d" % res.status_code)
