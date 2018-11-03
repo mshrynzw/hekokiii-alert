cb1df5f49d7dcf77cd98faa5b895323eb54801f9
mshrynzw <30368820+mshrynzw@users.noreply.github.com>
2018-11-03 23:19:42 +0900

    Add files via upload

---
+++ chk_movie.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import bs4
+import json
+import os
+import requests
+from datetime import datetime, timezone
+from time import sleep
+from tw_reserve import tweet_reserve_live
+
+def check_movie():
+
+    # YouTubeのチャンネルID
+    channelId = os.environ["YOUTUBE_CHANNEL_ID"]
+    # 「YouTube Data API (v3)」のAPIキー
+    key = os.environ["YOUTUBE_DATA_API_KEY"]
+
+    # 動画IDを格納
+    listVideoId = []
+
+    while True:
+
+        # UTC（協定世界時）を取得し、「YouTube Data API (v3)」より直近1日の新規動画情報（json）を取得
+        awareDT = datetime.now(timezone.utc).isoformat()
+        req = requests.get("https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=" + channelId + "&key=" + key + "&publishedAfter=" + awareDT[0:23] + "Z")
+        soup = bs4.BeautifulSoup(req.text, "lxml")
+        jsonText = soup.p.string
+        dictText = json.loads(jsonText)
+        listItems = dictText.get("items")
+
+        # 動画IDとタイトルを取得
+        for listValue in listItems:
+
+            videoId = listValue["id"]["videoId"]
+            title = listValue["snippet"]["title"]
+
+            if videoId not in listVideoId:
+                listVideoId.append(videoId)
+                strTweet = "【YouTube】「 #" + title + "」がアップロードされました。" + r"https://www.youtube.com/watch?v=" + videoId
+                tweet_reserve_live(strTweet)
+
+        sleep(10)
--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import threading
 from chk_start import check_start_live
 from chk_reserve import check_reserve_live
 from chk_notice import check_notice
-
+from chk_movie import check_movie
 
 if __name__ == "__main__":
     thread_1 = threading.Thread(target=check_start_live)
     thread_2 = threading.Thread(target=check_reserve_live)
     thread_3 = threading.Thread(target=check_notice)
+    thread_4 = threading.Thread(target=check_movie)
     thread_1.start()
     thread_2.start()
     thread_3.start()
+    thread_4.start()
