9469da895e323ebad110ca4fbbd6d40f850e6d75
mshrynzw <30368820+mshrynzw@users.noreply.github.com>
2019-02-14 20:23:48 +0900

    Add files via upload

--- proc_db.py
+++ proc_db.py
@@ -36,7 +36,7 @@ def db_check(cur, tableName, urlValue):
 
     sql = "SELECT COUNT(*) FROM {0} WHERE  url = '{1}'".format(tableName, urlValue)
     cur.execute(sql)
-    return cur
+    return cur.fetchone()
 
 # INSERT文
 def db_insert(cur, tableName, urlValue):
