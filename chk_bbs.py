87c9132911c20afdb37945b6d35b30b63530ef52
mshrynzw <30368820+mshrynzw@users.noreply.github.com>
2019-05-25 18:54:58 +0900

    Update chk_bbs.py

--- chk_bbs.py
+++ chk_bbs.py
@@ -10,7 +10,7 @@ from time import sleep
 from tw import proc_tweet
 
 # ログのフォーマットを定義
-logging.basicConfig(level=logging.WARNING, format='%(levelname)s : %(asctime)s : %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(levelname)s : %(asctime)s : %(message)s')
 # DBのテーブル名
 tblName = "bbs_comment_count"
 # 5chのURL
@@ -33,9 +33,9 @@ def check_bbs_count():
         driver.get(url_tmp)
         sleep(30)
 
-        elCntS = driver.find_elements_by_class_name("is-size-7")
-        elNameS = driver.find_elements_by_class_name("clname")
-        elCmtS = driver.find_elements_by_class_name("clmess")
+        elCntS = driver.find_elements_by_css_selector("h2.is-size-7")
+        elNameS = driver.find_elements_by_css_selector("span.clname")
+        elCmtS = driver.find_elements_by_css_selector("div.clmess")
 
         elCntList = []
         elNameList = []
