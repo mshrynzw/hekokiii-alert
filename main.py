1cb7a1692e8fddeb990024287fa04fc8de264c7d
mshrynzw <30368820+mshrynzw@users.noreply.github.com>
2019-08-31 20:27:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from chk_reserve import check_reserve_live
 from chk_notice import check_notice
 from chk_movie import check_movie
 from chk_bbs import check_bbs
+from chk_reserve_yt import check_reserve_yt
 
 if __name__ == "__main__":
     thread_1 = threading.Thread(target=check_start_live)
@@ -11,8 +12,11 @@ if __name__ == "__main__":
     thread_3 = threading.Thread(target=check_notice)
     thread_4 = threading.Thread(target=check_movie)
     thread_5 = threading.Thread(target=check_bbs)
+    thread_6 = threading.Thread(target=check_reserve_yt)
     thread_1.start()
     thread_2.start()
     thread_3.start()
     thread_4.start()
     thread_5.start()
+    thread_6.start()
+    
